1278b4b7e08e68fae7f78b3c97e652a336e5d754
bapungiri <bapun.k.giri@gmail.com>
2018-11-30 00:06:22 -0500

    initial prog

---
+++ miscel/plotCheck.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 29 22:06:17 2018
+
+@author: Bapun
+"""
+import csv
+
+#import matplotlib.pyplot as plt
+#plt.plot([1,2,3,4])
+#plt.ylabel('some numbers')
+#plt.show()
+
+#colors = ['red', 'blue', 'green']
+#print "colors[0]"
+
+filename= '../../DataGen/Advertising.csv'
+
+b = csv.reader(filename)  
