e862560437aa6d3aee4dc3052abae3c2d964e287
bapungiri <bapun.k.giri@gmail.com>
2019-03-10 11:01:33 -0400

    minor mods

---
+++ miscel/strinManpul.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  9 21:15:03 2019
+
+@author: Bapun
+"""
+
+sentI = input('What you type I will reverse the sentence: ')
+
+print(" ".join(sentI.split()[::-1]))
