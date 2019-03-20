24cdfd759f869479108827f9e9134f1319f0b132
Bapun giri <bapung@MSV00125.med.umich.edu>
2019-03-20 12:17:09 -0400

    reformatting code

--- spindleAnalysis/PFC-HippoSpindle.py
+++ spindleAnalysis/PFC-HippoSpindle.py
@@ -1,18 +1,5 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Mar 18 11:25:34 2019
-
-@author: bapung
-"""
-
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Mar 19 09:48:15 2019
-
-@author: bapung
-"""
+#-*- coding: utf-8 -*-
 
 
 import numpy as np
@@ -22,51 +9,52 @@ import matplotlib.pyplot as plt
 import scipy.stats as stats
 from scipy.ndimage.filters import gaussian_filter1d
 from OsCheck import DataDirPath, figDirPath
-#import scipy.signal as sg
-#import scipy.stats as stats
-#from scipy.signal import hilbert
+# import scipy.signal as sg
+# import scipy.stats as stats
+# from scipy.signal import hilbert
 import h5py
 import seaborn as sns
-#sns.set(style="darkgrid")
+
+# sns.set(style="darkgrid")
 
 
-#plt.style.use('seaborn')
+# plt.style.use('seaborn')
         
-colmapDark = plt.cm.Greys(np.linspace(0.35,1,3))
-colmapLight = plt.cm.Oranges(np.linspace(0.3,1,4))
+colmapDark = plt.cm.Greys(np.linspace(0.35, 1, 3))
+colmapLight = plt.cm.Oranges(np.linspace(0.3, 1, 4))
 
 sourceDir = DataDirPath() + 'sleep/'
-figFilename = figDirPath() +'SpindleAnalysis/PFCSpindle-HippSpindle_normSmoothed.pdf'
+figFilename = figDirPath() + 'SpindleAnalysis/PFCSpindle-HippSpindle_normSmoothed.pdf'
 
 arrays = {}
-f= h5py.File(sourceDir + 'sleep-basics.mat', 'r') 
+f = h5py.File(sourceDir + 'sleep-basics.mat', 'r')
 for k, v in f.items():
     arrays[k] = np.array(v)
 
 # spks = {}
 # fspikes= h5py.File(sourceDir + 'sleep-spikes.mat', 'r') 
-fbehav= h5py.File(sourceDir + 'sleep-behavior.mat', 'r') 
+fbehav = h5py.File(sourceDir + 'sleep-behavior.mat', 'r') 
 fripple = h5py.File(sourceDir + 'sleep-ripple.mat', 'r') 
-fspindle= h5py.File(sourceDir + 'sleep-spindle.mat', 'r') 
-#fICAStrength = h5py.File('/data/DataGen/ICAStrengthpy.mat', 'r') 
+fspindle = h5py.File(sourceDir + 'sleep-spindle.mat', 'r') 
+# fICAStrength = h5py.File('/data/DataGen/ICAStrengthpy.mat', 'r') 
 
 subjects = arrays['basics']
-#spikes = spks['spikes']
+# spikes = spks['spikes']
 
-#figFilename = figDirPath() +'PlaceCells.pdf'
-#pdf = PdfPages(figFilename)
-k=1
-m=[]
+# figFilename = figDirPath() +'PlaceCells.pdf'
+# pdf = PdfPages(figFilename)
+k = 1
+m = []
 plt.clf()
-#f, axarr = plt.subplots(2, 4)
+# f, axarr = plt.subplots(2, 4)
 
-for sub in range(0,19):
+for sub in range(0, 19):
     sub_name = subjects[sub]
     print(sub_name)
     
     area = list((fspindle['spindle'][sub_name]).keys())
     
-    if len(area)>1 :
+    if len(area) > 1:
         
         pfcSpindle = np.transpose(fspindle['spindle'][sub_name]['CTX']['peakTime'][:])
         hpcSpindle = np.transpose(fspindle['spindle'][sub_name]['HPC']['peakTime'][:])
@@ -74,16 +62,16 @@ for sub in range(0,19):
         
         diffTime = ((pfcSpindle.transpose() - hpcSpindle).ravel())/1e6
 #        diffTime = ((hpcRipple - pfcSpindle).ravel())/1e6
-        hist, edge = np.histogram(diffTime, bins= np.arange(-30,30,1))
-        hist = gaussian_filter1d(stats.zscore(hist), sigma = 2)
+        hist, edge = np.histogram(diffTime, bins=np.arange(-30, 30, 1))
+        hist = gaussian_filter1d(stats.zscore(hist), sigma=2)
         
         if 'Steve' in sub_name:
             
-            m.append(plt.subplot(1,2,1))
+            m.append(plt.subplot(1, 2, 1))
             
             if 'Sleep' in sub_name:
             
-                plt.plot(edge[0:len(hist)], hist, color='#f49e42', alpha= 0.95- int(sub_name[-1])/10, label = 'Light'+ sub_name[-1])
+                plt.plot(edge[0:len(hist)], hist, color='#f49e42', alpha=0.95-int(sub_name[-1])/10, label = 'Light'+ sub_name[-1])
                            
             else:
 
@@ -115,7 +103,8 @@ for pltind in [0,1]:
 for pltind in [0]:
     m[pltind].set(ylabel='HPC spindle events (zscored)')      
 
-plt.suptitle('PFC-Spindle vs HPC-Spindle')       
+plt.suptitle('PFC-Spindle vs HPC-Spindle') 
+
 #plt.savefig(figFilename, dpi = 300)
     
     
