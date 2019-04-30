7e7fe40a9c99da242f5cb8b0abb0ba77df988217
Bapun giri <bapung@MSV00125.med.umich.edu>
2019-04-30 11:36:07 -0400

    open ephys to dat conversion minor mods

--- fileUtils/OpenEphystoDat.py
+++ fileUtils/OpenEphystoDat.py
@@ -21,8 +21,9 @@ import readOpenEphys as openEphys
 #import seaborn as sns
 
 
-sourceDir = RawDataPath() + 'SleepDeprivation/Beatrice/PRE/2019-04-22_04-10-57/'
-filename = sourceDir + '100_ADC4.continuous'
+sourceDir = RawDataPath() + 'SleepDeprivation/Beatrice/PRE/2019-04-22_04-10-57/'   #PRE
+#sourceDir = RawDataPath() + 'SleepDeprivation/Beatrice/POST/2019-04-22_08-24-45/'   #POST
+filename = sourceDir + '100_CH10.continuous'
 
 
 data= openEphys.load(filename)
@@ -30,12 +31,39 @@ data= openEphys.load(filename)
 lfp = data['data']
 eeg = lfp[0:-1:24]
 
-#f,t,spect = sg.spectrogram(eeg, 1250,nperseg=2048*4, noverlap=2048*3)
-#spect = gf.gaussian_filter(spect, sigma= 1)
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * 1250
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = sg.butter(order, [low, high], btype='band')
+    return b, a
+
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = sg.lfilter(b, a, data)
+    return y
+
+lowcut = 140
+highcut = 250
+
+y = butter_bandpass_filter(eeg, lowcut, highcut, 1250, order=6)
+
+
+#b, a = sg.butter(4, 100, 'low', analog=True)
+#sos = sg.butter(3, 100, btype = 'bandpass', fs=1250, output='sos')
+#yf = sg.sosfilt(sos,eegnrem1)
+
+
+
+f,t,spect = sg.spectrogram(eeg, 1250,nperseg=2048*4, noverlap=2048*3)
+spect = gf.gaussian_filter(spect, sigma= 1.2)
 
 plt.clf()
 plt.plot(eeg)
-#plt.pcolormesh(t, f[10:575], spect[10:575,:],cmap = 'hot', vmin=60, vmax=2050)
+plt.plot(y)
+#plt.pcolormesh(t/3600, f[0:575], spect[0:575,:],cmap = 'hot', vmin=60, vmax=2550)
 #with open(filename, 'rb') as f:
 #        # Read header info, file length, and number of records
 #        header = f.read(1024)
