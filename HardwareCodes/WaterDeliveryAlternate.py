d5e1ae55f8694b6aefed284d0594765fb081e61d
Bapun giri <bapung@MSV00125.med.umich.edu>
2019-05-06 17:43:13 -0400

    minor mods to water delivery system

--- HardwareCodes/WaterDeliveryAlternate.py
+++ HardwareCodes/WaterDeliveryAlternate.py
@@ -15,9 +15,43 @@ GPIO.setwarnings(False)
 #=======Setting up I/O===========
 
 GPIO.setup(18,GPIO.OUT)
+GPIO.setup(23,GPIO.OUT)
+
+GPIO.setup(22,GPIO.IN, pull_up_down= GPIO.PUD_down)
+GPIO.setup(7,GPIO.IN)
+
+flag = 0
+
+try:
+
+    while True:
+
+        if flag == 0 and GPIO.input(22):
+            GPIO.output(18,GPIO.HIGH)
+            time.sleep(0.100)
+            GPIO.output(18,GPIO.LOW)
+            flag==2
+
+        if flag == 0 and GPIO.input(7):
+            GPIO.output(18,GPIO.HIGH)
+            time.sleep(0.100)
+            GPIO.output(18,GPIO.LOW)
+            flag==1
 
 
-GPIO.output(18,GPIO.HIGH)
-time.sleep(1)
-GPIO.output(18,GPIO.LOW)
-time.sleep(1)
+
+
+        if flag == 1 and GPIO.input(22):
+            GPIO.output(18,GPIO.HIGH)
+            time.sleep(0.100)
+            GPIO.output(18,GPIO.LOW)
+            flag==2
+
+        if flag == 2 and GPIO.input(7):
+            GPIO.output(23,GPIO.HIGH)
+            time.sleep(0.100)
+            GPIO.output(23,GPIO.LOW)
+            flag==1
+
+finally:
+    GPIO.cleanup()
