4434081852dc4ab2111fa0b9f0ed0015d00c9591
Bapun giri <bapung@MSV00125.med.umich.edu>
2019-08-15 15:04:12 -0400

    extraction modified

--- BasicAnalysis/positionExtract.py
+++ BasicAnalysis/positionExtract.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 filename = '/data/Clustering/SleepDeprivation/RatJ/Behavior_Position/Take 2019-05-31 03.55.25 AM.fbx'
 
 f = open(filename, 'r')
-# data = pd.read_csv(filename, header=5)
+# data = pd.read_csv(filename, header=5, skipfooter=800)
 
 # time = data['Time (Seconds)'].tolist()
 # x = data['X'].tolist()
@@ -16,14 +16,28 @@ f = open(filename, 'r')
 
 # plt.plot(x, z, '.')
 
-i = 1
+
+# i = 1
 k = 830
 xpos, ypos, zpos = [], [], []
 with open(filename) as f:
-    next(f)
-    for line in f:
-        i = i+1
-        if i > k:
+    # next(f)
+    for i, line in enumerate(f):
+
+        m = ''.join(line)
+
+        if 'RawSegs' in m:
+            track_begin = i+2
+
+        if 'Segs' in m:
+            track_end = i-1
+
+
+with open(filename) as f:
+    # next(f)
+    for i, line in enumerate(f):
+
+        if i > track_begin:
             line = line.strip()
             m = line.split(',')
 
@@ -31,10 +45,13 @@ with open(filename) as f:
             ypos.append(m[2])
             zpos.append(m[3])
 
-        if i > (k+120*60*60*2):
+        if i > track_end:
             break
+
 
 xpos = list(map(float, xpos))
 ypos = list(map(float, ypos))
 zpos = list(map(float, zpos))
+
+plt.clf()
 plt.plot(zpos)
