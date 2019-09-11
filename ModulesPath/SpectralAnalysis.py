5f749cb04869b9aae7c76bc42ae3859c46c0635c
Bapun giri <bapung@MSV00125.med.umich.edu>
2019-09-11 17:13:39 -0400

    ripple detection and ripple channels modified

--- ModulesPath/SpectralAnalysis.py
+++ ModulesPath/SpectralAnalysis.py
@@ -15,28 +15,30 @@ import os
 
 def lfpSpectrogram(basePath, sRate, nChans, reqChan, loadfrom=0):
 
-    duration = 3600*10
+    duration = 3600 * 10
     offsetP = 0
     subname = os.path.basename(os.path.normpath(basePath))
 
     if loadfrom == 1:
-        fileName = basePath + subname + '_BestThetaChan.npy'
+        fileName = basePath + subname + "_BestThetaChan.npy"
         BestThetaInfo = np.load(fileName)
         eegChan = BestThetaInfo
 
     else:
-        fileName = basePath + subname + '.eeg'
+        fileName = basePath + subname + ".eeg"
 
-        b1 = np.memmap(fileName, dtype='int16', mode='r',
-                       offset=int(offsetP) * nChans * 2 +
-                       1 * (reqChan - 1) * 2)
+        b1 = np.memmap(
+            fileName,
+            dtype="int16",
+            mode="r",
+            offset=int(offsetP) * nChans * 2 + 1 * (reqChan - 1) * 2,
+        )
         eegChan = b1[0::nChans]
 
-    sos = sg.butter(3, 100, btype='low', fs=sRate, output='sos')
+    sos = sg.butter(3, 100, btype="low", fs=sRate, output="sos")
     yf = sg.sosfilt(sos, eegChan)
-    f, t, x = sg.spectrogram(yf, fs=sRate, nperseg=5*1250,
-                             noverlap=3*1250)
-    sample_data = yf[0:sRate*5]
+    f, t, x = sg.spectrogram(yf, fs=sRate, nperseg=5 * 1250, noverlap=3 * 1250)
+    sample_data = yf[0 : sRate * 5]
     # yf = ft.fft(yf) / len(eegChan)
     # xf = np.linspace(0.0, SampFreq / 2, len(eegChan) // 2)
     # y1 = 2.0 / (len(xf)) * np.abs(yf[:len(eegChan) // 2])
@@ -47,24 +49,30 @@ def lfpSpectrogram(basePath, sRate, nChans, reqChan, loadfrom=0):
 
 def lfpSpectMaze(sub_name, nREMPeriod, RecInfo, channel):
 
-    SampFreq = RecInfo['samplingFrequency']
-    frames = RecInfo['behavFrames']
-    behav = RecInfo['behav']
-    nChans = RecInfo['numChannels']
-#    ReqChan = RecInfo['SpectralChannel']
+    SampFreq = RecInfo["samplingFrequency"]
+    frames = RecInfo["behavFrames"]
+    behav = RecInfo["behav"]
+    nChans = RecInfo["numChannels"]
+    #    ReqChan = RecInfo['SpectralChannel']
     ReqChan = channel
     duration = 5  # chunk of lfp in seconds
 
-    offsetP = ((nREMPeriod - behav[1, 0]) // 1e6) * \
-        SampFreq + int(np.diff(frames[0, :]))
-    b1 = np.memmap('/data/EEGData/' + sub_name + '.eeg', dtype='int16', mode='r',
-                   offset=int(offsetP) * nChans * 2 + 1 * (ReqChan - 1) * 2, shape=(1, nChans * SampFreq * duration))
+    offsetP = ((nREMPeriod - behav[1, 0]) // 1e6) * SampFreq + int(
+        np.diff(frames[0, :])
+    )
+    b1 = np.memmap(
+        "/data/EEGData/" + sub_name + ".eeg",
+        dtype="int16",
+        mode="r",
+        offset=int(offsetP) * nChans * 2 + 1 * (ReqChan - 1) * 2,
+        shape=(1, nChans * SampFreq * duration),
+    )
     eegChan = b1[0, ::nChans]
-    sos = sg.butter(3, 100, btype='low', fs=SampFreq, output='sos')
+    sos = sg.butter(3, 100, btype="low", fs=SampFreq, output="sos")
     yf = sg.sosfilt(sos, eegChan)
     yf = ft.fft(yf) / len(eegChan)
     xf = np.linspace(0.0, SampFreq / 2, len(eegChan) // 2)
-    y1 = 2.0 / (len(xf)) * np.abs(yf[:len(eegChan) // 2])
+    y1 = 2.0 / (len(xf)) * np.abs(yf[: len(eegChan) // 2])
     y1 = smth.gaussian_filter(y1, 8)
 
     return y1, xf
@@ -77,22 +85,28 @@ def bestThetaChannel(basePath, sampleRate, nChans, badChannels, saveThetaChan=0)
 
     """
 
-    badChannels = [x-1 for x in badChannels]  # zero indexing correction
+    badChannels = [x - 1 for x in badChannels]  # zero indexing correction
     duration = 3600  # chunk of lfp in seconds
     nyq = 0.5 * sampleRate
     lowTheta = 5
     highTheta = 10
     subname = os.path.basename(os.path.normpath(basePath))
-    fileName = basePath + subname + '.eeg'
+    fileName = basePath + subname + ".eeg"
 
-    lfpCA1 = np.memmap(fileName, dtype='int16', mode='r',
-                       shape=(sampleRate * duration, nChans))
+    lfpCA1 = np.memmap(
+        fileName, dtype="int16", mode="r", shape=(sampleRate * duration, nChans)
+    )
 
-#    goodChannels = [i for i in range(len(a)) if a[i]==1]
-#    lfpCA1[:,goodChannels-1]
+    #    goodChannels = [i for i in range(len(a)) if a[i]==1]
+    #    lfpCA1[:,goodChannels-1]
 
-    sos = sg.butter(3, [lowTheta/nyq, highTheta/nyq],
-                    btype='bandpass', output='sos', fs=sampleRate)
+    sos = sg.butter(
+        3,
+        [lowTheta / nyq, highTheta / nyq],
+        btype="bandpass",
+        output="sos",
+        fs=sampleRate,
+    )
     yf = sg.sosfiltfilt(sos, lfpCA1, axis=0)
 
     avgTheta = np.mean(np.square(yf), axis=0)
@@ -106,48 +120,62 @@ def bestThetaChannel(basePath, sampleRate, nChans, badChannels, saveThetaChan=0)
 
     if saveThetaChan == 1:
         reqChan = bestChannels[0]
-        b1 = np.memmap(fileName, dtype='int16', mode='r')
+        b1 = np.memmap(fileName, dtype="int16", mode="r")
         ThetaExtract = b1[reqChan::nChans]
-        ThetaExtract2 = b1[reqChan-16::nChans]
+        ThetaExtract2 = b1[reqChan - 16 :: nChans]
 
-        np.save(basePath+subname+'_BestThetaChan.npy', ThetaExtract)
-        np.save(basePath+subname+'_BestThetaChan.npy', ThetaExtract2)
+        np.save(basePath + subname + "_BestThetaChan.npy", ThetaExtract)
+        np.save(basePath + subname + "_BestThetaChan.npy", ThetaExtract2)
 
     return bestChannels
 
 
-def bestRippleChannel(fileName, sampleRate, nChans, badChannels):
+def bestRippleChannel(basePath, sampleRate, nChans, badChannels, saveRippleChan=1):
     """
     fileName: name of the .eeg file
     sampleRate: sampling frequency of eeg;
 
     """
 
-    badChannels = [x-1 for x in badChannels]  # zero indexing correction
-    duration = 60*30  # chunk of lfp in seconds
+    badChannels = [x - 1 for x in badChannels]  # zero indexing correction
+    duration = 60 * 30  # chunk of lfp in seconds
     nyq = 0.5 * sampleRate  # Nyquist frequency for sampling rate
-    lowRipple = 150
-    highRipple = 250
+    lowRipple = 150  # ripple lower end frequency in Hz
+    highRipple = 250  # ripple higher end frequency in Hz
+    subname = os.path.basename(os.path.normpath(basePath))
+    fileName = basePath + subname + ".eeg"
 
-    lfpCA1 = np.memmap(fileName, dtype='int16', mode='r',
-                       shape=(sampleRate * duration, nChans))
+    lfpCA1 = np.memmap(
+        fileName, dtype="int16", mode="r", shape=(sampleRate * duration, nChans)
+    )
 
-#    goodChannels = [i for i in range(len(a)) if a[i]==1]
-#    lfpCA1[:,goodChannels-1]
+    #    goodChannels = [i for i in range(len(a)) if a[i]==1]
+    #    lfpCA1[:,goodChannels-1]
 
-    b, a = sg.butter(3, [lowRipple/nyq, highRipple/nyq],
-                     btype='bandpass')
+    b, a = sg.butter(3, [lowRipple / nyq, highRipple / nyq], btype="bandpass")
     delta = sg.filtfilt(b, a, lfpCA1, axis=0)
 
-# Hilbert transform for calculating signal's envelope
+    # Hilbert transform for calculating signal's envelope
     analytic_signal = sg.hilbert(delta)
     amplitude_envelope = np.abs(analytic_signal)
 
     # rms_signal = np.sqrt(np.mean(yf**2))
     avgRipple = np.mean(amplitude_envelope, axis=0)
     idx = np.argsort(avgRipple)
-
     bestChannels = np.setdiff1d(idx, badChannels, assume_unique=True)[::-1]
+
+    if saveRippleChan == 1:
+        bestChan = bestChannels[0]
+        best2ndChan = bestChannels[1]
+
+        b1 = np.memmap(fileName, dtype="int16", mode="r")
+        RipplelfpExtract = b1[bestChan::nChans]
+        Ripple2ndlfpExtract = b1[best2ndChan::nChans]
+
+        Ripplelfps = {"BestChan": RipplelfpExtract, "Best2ndChan": Ripple2ndlfpExtract}
+
+        np.save(basePath + subname + "_BestRippleChans.npy", Ripplelfps)
+        # np.save(basePath + subname + "_BestRippleChan.npy", RippleExtract2)
 
     # selecting first three channels
 
--- ModulesPath/lfpDetect.py
+++ ModulesPath/lfpDetect.py
@@ -12,28 +12,37 @@ import scipy.fftpack as ft
 import scipy.ndimage as smth
 import scipy.stats as stat
 import numpy.random as rnd
+import os
 
 
-def swr(lfpfile, RippleChannel, samplingFrequency, numChans):
+def swr(lfpfile, sRate):
 
-    lfp = lfpfile
-    SampFreq = samplingFrequency
-    nChans = numChans
-    ReqChan = RippleChannel
+    basePath = lfpfile
+    SampFreq = sRate
+    # nChans = numChans
+    # ReqChan = RippleChannel
     nyq = 0.5 * SampFreq
-    offsetp = (ReqChan-1)*2
-    duration = 3600*14
+    # offsetp = (ReqChan - 1) * 2
+    # duration = 3600 * 14
     lowthresholdFactor = 1
     highThresholdFactor = 2
-    # print(duration)
+    subname = os.path.basename(os.path.normpath(basePath))
+    fileName = basePath + subname + "_BestRippleChans.npy"
+    lfpCA1 = np.load(fileName)
 
     # loading the required chanel from eeg file for ripple detection
-    lfpCA1 = np.memmap(lfp, dtype='int16', mode='r',
-                       offset=offsetp, shape=(SampFreq * duration, nChans))
-    signal = lfpCA1[:, 0]
+    # lfpCA1 = np.memmap(
+    #     lfp,
+    #     dtype="int16",
+    #     mode="r",
+    #     offset=offsetp,
+    #     shape=(SampFreq * duration, nChans),
+    # )
+    signal = lfpCA1.item()
+    signal = signal["BestChan"]
     signal = np.array(signal, dtype=np.float)  # convert data to float
 
-    b, a = sg.butter(3, [150/nyq, 240/nyq], btype='bandpass')
+    b, a = sg.butter(3, [150 / nyq, 240 / nyq], btype="bandpass")
     yf = sg.filtfilt(b, a, signal)
 
     squared_signal = np.square(yf)
@@ -43,14 +52,15 @@ def swr(lfpfile, RippleChannel, samplingFrequency, numChans):
     # analytic_signal = sg.hilbert(yf)
     # amplitude_envelope = stat.zscore(np.abs(analytic_signal))
 
-    windowLength = SampFreq/SampFreq*11
-    window = np.ones((int(windowLength),))/windowLength
+    windowLength = SampFreq / SampFreq * 11
+    window = np.ones((int(windowLength),)) / windowLength
 
     smoothSignal = sg.filtfilt(window, 1, squared_signal, axis=0)
     zscoreSignal = stat.zscore(smoothSignal)
 
     hist_zscoresignal, edges_zscoresignal = np.histogram(
-        zscoreSignal, bins=np.linspace(0, 6, 100))
+        zscoreSignal, bins=np.linspace(0, 6, 100)
+    )
 
     ThreshSignal = np.diff(np.where(zscoreSignal > lowthresholdFactor, 1, 0))
     start_ripple = np.argwhere(ThreshSignal == 1)
@@ -60,7 +70,7 @@ def swr(lfpfile, RippleChannel, samplingFrequency, numChans):
     firstPass = np.concatenate((start_ripple, stop_ripple), axis=1)
 
     # ===== merging close ripples
-    minInterRippleSamples = 30/1000*SampFreq
+    minInterRippleSamples = 30 / 1000 * SampFreq
     secondPass = []
     ripple = firstPass[0]
     for i in range(1, len(firstPass)):
@@ -79,14 +89,14 @@ def swr(lfpfile, RippleChannel, samplingFrequency, numChans):
     peakNormalizedPower = []
 
     for i in range(0, len(secondPass)):
-        maxValue = max(zscoreSignal[secondPass[i, 0]:secondPass[i, 1]])
+        maxValue = max(zscoreSignal[secondPass[i, 0] : secondPass[i, 1]])
         if maxValue > highThresholdFactor:
             thirdPass.append(secondPass[i])
             peakNormalizedPower.append(maxValue)
 
     thirdPass = np.asarray(thirdPass)
 
-    ripple_duration = np.diff(thirdPass, axis=1)/1250*1000
+    ripple_duration = np.diff(thirdPass, axis=1) / 1250 * 1000
 
     # delete very short ripples
     shortRipples = np.where(ripple_duration < 20)[0]
@@ -95,43 +105,54 @@ def swr(lfpfile, RippleChannel, samplingFrequency, numChans):
     # delete very short ripples
     shortRipples = np.where(ripple_duration < 20)[0]
     thirdPass = np.delete(thirdPass, shortRipples, 0)
+
+    # TODO delete very long ripples
+
+    # TODO delet sharp ripple like artifacts
 
     # selecting some example ripples
-    idx = rnd.randint(0, thirdPass.shape[0], 5, dtype='int')
+    idx = rnd.randint(0, thirdPass.shape[0], 5, dtype="int")
     example_ripples = []
     example_ripples_duration = []  # in frames
     for i in range(5):
         example_ripples.append(
-            signal[thirdPass[idx[i], 0]-125:thirdPass[idx[i], 1]+125])
-        example_ripples_duration.append(
-            thirdPass[idx[i], 1]-thirdPass[idx[i], 0])
+            signal[thirdPass[idx[i], 0] - 125 : thirdPass[idx[i], 1] + 125]
+        )
+        example_ripples_duration.append(thirdPass[idx[i], 1] - thirdPass[idx[i], 0])
 
     other_measures = dict()
-    other_measures['example_ripples'] = [
-        example_ripples, example_ripples_duration]
-    other_measures['zscore_dist'] = [hist_zscoresignal, edges_zscoresignal]
+    other_measures["example_ripples"] = [example_ripples, example_ripples_duration]
+    other_measures["zscore_dist"] = [hist_zscoresignal, edges_zscoresignal]
 
     return thirdPass, other_measures
 
 
 def deltawave(sub_name, nREMPeriod, RecInfo):
 
-    SampFreq = RecInfo['samplingFrequency']
-    frames = RecInfo['behavFrames']
-    behav = RecInfo['behav']
-    nChans = RecInfo['numChannels']
-    ReqChan = RecInfo['SpectralChannel']
+    SampFreq = RecInfo["samplingFrequency"]
+    frames = RecInfo["behavFrames"]
+    behav = RecInfo["behav"]
+    nChans = RecInfo["numChannels"]
+    ReqChan = RecInfo["SpectralChannel"]
 
-    offsetP = ((nREMPeriod-behav[2, 0])//1e6)*SampFreq + \
-        int(np.diff(frames[0, :]))+int(np.diff(frames[1, :]))
-    b1 = np.memmap('/data/EEGData/' + sub_name + '.eeg', dtype='int16', mode='r',
-                   offset=int(offsetP)*nChans*2 + 1*(ReqChan-1)*2, shape=(1, nChans*SampFreq*5))
+    offsetP = (
+        ((nREMPeriod - behav[2, 0]) // 1e6) * SampFreq
+        + int(np.diff(frames[0, :]))
+        + int(np.diff(frames[1, :]))
+    )
+    b1 = np.memmap(
+        "/data/EEGData/" + sub_name + ".eeg",
+        dtype="int16",
+        mode="r",
+        offset=int(offsetP) * nChans * 2 + 1 * (ReqChan - 1) * 2,
+        shape=(1, nChans * SampFreq * 5),
+    )
     eegnrem1 = b1[0, ::nChans]
-    sos = sg.butter(3, 100, btype='low', fs=SampFreq, output='sos')
+    sos = sg.butter(3, 100, btype="low", fs=SampFreq, output="sos")
     yf = sg.sosfilt(sos, eegnrem1)
-    yf = ft.fft(yf)/len(eegnrem1)
-    xf = np.linspace(0.0, SampFreq/2, len(eegnrem1)//2)
-    y1 = 2.0/(len(xf)) * np.abs(yf[:len(eegnrem1)//2])
+    yf = ft.fft(yf) / len(eegnrem1)
+    xf = np.linspace(0.0, SampFreq / 2, len(eegnrem1) // 2)
+    y1 = 2.0 / (len(xf)) * np.abs(yf[: len(eegnrem1) // 2])
     y1 = smth.gaussian_filter(y1, 8)
 
     return y1, xf
@@ -139,22 +160,30 @@ def deltawave(sub_name, nREMPeriod, RecInfo):
 
 def spindle(sub_name, nREMPeriod, RecInfo):
 
-    SampFreq = RecInfo['samplingFrequency']
-    frames = RecInfo['behavFrames']
-    behav = RecInfo['behav']
-    nChans = RecInfo['numChannels']
-    ReqChan = RecInfo['SpectralChannel']
+    SampFreq = RecInfo["samplingFrequency"]
+    frames = RecInfo["behavFrames"]
+    behav = RecInfo["behav"]
+    nChans = RecInfo["numChannels"]
+    ReqChan = RecInfo["SpectralChannel"]
 
-    offsetP = ((nREMPeriod-behav[2, 0])//1e6)*SampFreq + \
-        int(np.diff(frames[0, :]))+int(np.diff(frames[1, :]))
-    b1 = np.memmap('/data/EEGData/' + sub_name + '.eeg', dtype='int16', mode='r',
-                   offset=int(offsetP)*nChans*2 + 1*(ReqChan-1)*2, shape=(1, nChans*SampFreq*5))
+    offsetP = (
+        ((nREMPeriod - behav[2, 0]) // 1e6) * SampFreq
+        + int(np.diff(frames[0, :]))
+        + int(np.diff(frames[1, :]))
+    )
+    b1 = np.memmap(
+        "/data/EEGData/" + sub_name + ".eeg",
+        dtype="int16",
+        mode="r",
+        offset=int(offsetP) * nChans * 2 + 1 * (ReqChan - 1) * 2,
+        shape=(1, nChans * SampFreq * 5),
+    )
     eegnrem1 = b1[0, ::nChans]
-    sos = sg.butter(3, 100, btype='low', fs=SampFreq, output='sos')
+    sos = sg.butter(3, 100, btype="low", fs=SampFreq, output="sos")
     yf = sg.sosfilt(sos, eegnrem1)
-    yf = ft.fft(yf)/len(eegnrem1)
-    xf = np.linspace(0.0, SampFreq/2, len(eegnrem1)//2)
-    y1 = 2.0/(len(xf)) * np.abs(yf[:len(eegnrem1)//2])
+    yf = ft.fft(yf) / len(eegnrem1)
+    xf = np.linspace(0.0, SampFreq / 2, len(eegnrem1) // 2)
+    y1 = 2.0 / (len(xf)) * np.abs(yf[: len(eegnrem1) // 2])
     y1 = smth.gaussian_filter(y1, 8)
 
     return y1, xf
@@ -162,22 +191,30 @@ def spindle(sub_name, nREMPeriod, RecInfo):
 
 def sharpWaveOnly(sub_name, nREMPeriod, RecInfo):
 
-    SampFreq = RecInfo['samplingFrequency']
-    frames = RecInfo['behavFrames']
-    behav = RecInfo['behav']
-    nChans = RecInfo['numChannels']
-    ReqChan = RecInfo['SpectralChannel']
+    SampFreq = RecInfo["samplingFrequency"]
+    frames = RecInfo["behavFrames"]
+    behav = RecInfo["behav"]
+    nChans = RecInfo["numChannels"]
+    ReqChan = RecInfo["SpectralChannel"]
 
-    offsetP = ((nREMPeriod-behav[2, 0])//1e6)*SampFreq + \
-        int(np.diff(frames[0, :]))+int(np.diff(frames[1, :]))
-    b1 = np.memmap('/data/EEGData/' + sub_name + '.eeg', dtype='int16', mode='r',
-                   offset=int(offsetP)*nChans*2 + 1*(ReqChan-1)*2, shape=(1, nChans*SampFreq*5))
+    offsetP = (
+        ((nREMPeriod - behav[2, 0]) // 1e6) * SampFreq
+        + int(np.diff(frames[0, :]))
+        + int(np.diff(frames[1, :]))
+    )
+    b1 = np.memmap(
+        "/data/EEGData/" + sub_name + ".eeg",
+        dtype="int16",
+        mode="r",
+        offset=int(offsetP) * nChans * 2 + 1 * (ReqChan - 1) * 2,
+        shape=(1, nChans * SampFreq * 5),
+    )
     eegnrem1 = b1[0, ::nChans]
-    sos = sg.butter(3, 100, btype='low', fs=SampFreq, output='sos')
+    sos = sg.butter(3, 100, btype="low", fs=SampFreq, output="sos")
     yf = sg.sosfilt(sos, eegnrem1)
-    yf = ft.fft(yf)/len(eegnrem1)
-    xf = np.linspace(0.0, SampFreq/2, len(eegnrem1)//2)
-    y1 = 2.0/(len(xf)) * np.abs(yf[:len(eegnrem1)//2])
+    yf = ft.fft(yf) / len(eegnrem1)
+    xf = np.linspace(0.0, SampFreq / 2, len(eegnrem1) // 2)
+    y1 = 2.0 / (len(xf)) * np.abs(yf[: len(eegnrem1) // 2])
     y1 = smth.gaussian_filter(y1, 8)
 
     return y1, xf
