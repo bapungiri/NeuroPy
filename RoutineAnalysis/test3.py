80cbc52bf5d0e03e577f24d2d5cba76dfafd2443
Bapun giri <bapung@MSV00125.med.umich.edu>
2020-01-16 18:03:42 -0500

    template prm and prb added

---
+++ RoutineAnalysis/test3.py
@@ -0,0 +1,12 @@
+# number = int(input("Enter a number: "))
+
+
+for number in range(8, 15):
+    if number > 10:
+        print("Number is greater than 10")
+
+    if number > 11:
+        print("Number is greater than 10")
+
+    else:
+        print("not greater")
