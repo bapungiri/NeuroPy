e8ed9426cb762628d463fb4d1e30bb7b0241c67c
Bapun giri <bapung@MSV00125.med.umich.edu>
2020-02-03 14:35:00 -0500

    modified positon files

--- ModulesPath/getPosition.py
+++ ModulesPath/getPosition.py
@@ -8,6 +8,72 @@ import time
 from datetime import datetime
 
 
+def posPreprocess(basePath):
+
+    for file in os.listdir(basePath):
+        if file.endswith(".eeg"):
+            subname = file[:-4]
+
+    nframes = np.load(basePath / "og_files" / "numframes_OG.npy")
+    DatFileOG = basePath / "RatJ_Day1_2019-05-31_03-55-36.dat"
+    endFrametime = len(np.memmap(DatFileOG, dtype="int16", mode="r")) / (75 * 30000)
+
+    noisyFrames = np.load(basePath / "og_files" / "noisy_timestamps_fromOG.npy")
+    posInfo = np.load(
+        basePath / "og_files" / "RatJ_Day1_2019-05-31_03-55-36_position_OG.npy",
+        allow_pickle=True,
+    )
+
+    posX = np.asarray(posInfo.item().get("X"))
+    posY = np.asarray(posInfo.item().get("Y"))
+    frames = posInfo.item().get("frames")
+    video_starttime = posInfo.item().get("begin")
+    video_duration = len(posX) / (120 * 3600)
+    video_endtime = video_starttime + timedelta(hours=video_duration)
+
+    ephys_starttime = datetime.strptime(subname[-19:], "%Y-%m-%d_%H-%M-%S")
+
+    time_diff = ephys_starttime - video_starttime
+
+    ephys_duration = nframes / (3600 * 1250)
+    ephys_endtime = ephys_starttime + timedelta(hours=ephys_duration)
+    time_record = np.arange(ephys_starttime, ephys_endtime, dtype="datetime64[h]")
+    time_diff_end = video_endtime - ephys_endtime
+
+    # start = pd.Timestamp(ephys_starttime)
+    # end = pd.Timestamp(ephys_endtime)
+    # tim2 = pd.to_datetime(np.linspace(start.value, end.value, nframes))
+    # tim2 = np.asarray(tim2)
+    # plt.plot(posY)
+
+    t_ephys = np.arange(0, nframes) / 1250
+    t_video = np.linspace(
+        -time_diff.total_seconds(),
+        nframes / 1250 + time_diff_end.total_seconds(),
+        len(posX),
+    )
+
+    noisy_time = noisyFrames / 1250
+    t_video_noisy = np.concatenate(
+        [np.where(np.digitize(t_video, x) == 1)[0] for x in noisy_time], axis=0
+    )
+
+    ind_good = np.setdiff1d(np.arange(1, len(posX)), t_video_noisy)
+    t_video_keep = t_video[ind_good]
+    posX_keep = posX[ind_good]
+    posY_keep = posY[ind_good]
+    t_video_keep = t_video_keep[(0 <= t_video_keep) & (t_video_keep <= nframes / 1250)]
+
+    posVar = {}
+    posVar["X"] = posX_keep
+    posVar["Y"] = posY_keep
+    posVar["time_orig"] = t_video_keep
+    posVar["time"] = np.arange(0, len(t_video_keep)) / 120
+    # posVar["begin"] = self.tbegin
+    # posVar["pos_sRate"] = self.optitrack_sRate
+    # np.save(Path(basePath, subname + "_position.npy"), posVar)
+
+
 class ExtractPosition:
 
     optitrack_sRate = 120  # position sample rate
@@ -29,8 +95,8 @@ class ExtractPosition:
             posInfo = np.load(self.basePath + "_positon.npy", allow_pickle=True)
             self.posX = posInfo.item().get("X")  # in seconds
             self.posZ = posInfo.item().get("Y")  # in seconds
-            self.frames = posInfo.item().get("frames")  # in seconds
-            self.tbegin = posInfo.item().get("begin")
+            self.time = posInfo.item().get("time")  # in seconds
+            # self.tbegin = posInfo.item().get("begin")
 
         # Run to generate new position file
         else:
@@ -186,7 +252,7 @@ class ExtractPosition:
 
         plt.clf()
         plt.setp(plt.gca(), autoscale_on=True)
-        plt.plot(self.frames[::4], self.posZ[::4])
+        plt.plot(self.time[::4], self.posZ[::4])
 
         tellme("You will define a rectangle for track, click to begin")
 
@@ -216,8 +282,8 @@ class ExtractPosition:
             for p in ph:
                 p.remove()
         self.corrds = pts
-        self.maze_start = (int(pts[0][0])) / self.optitrack_sRate  # in seconds
-        self.maze_end = int(pts[2][0]) / self.optitrack_sRate  # in seconds
+        self.maze_start = pts[0][0]  # in seconds
+        self.maze_end = pts[2][0]  # in seconds
 
         pre_time = np.array([0, self.maze_start - 1])
         maze_time = np.array([self.maze_start, self.maze_end])
