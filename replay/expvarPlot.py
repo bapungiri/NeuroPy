cae9b0c4f9f50440c4c25b73bec3a237fcc5877b
Bapun giri <bapung@MSV00125.med.umich.edu>
2020-04-29 18:33:44 -0400

    plotted ratn expvar with sleep background

--- replay/expvarPlot.py
+++ replay/expvarPlot.py
@@ -41,8 +41,20 @@ fig.subplots_adjust(hspace=0.4)
 ax1 = fig.add_subplot(gs[0, 0])
 t = (np.linspace(0, 40, 41) * 0.25)[1:] - 0.125
 sessions[0].brainstates.addBackgroundtoPlots(ax1)
-ax1.plot(t, ev1.squeeze(), "k")
-ax1.plot(t, rev1.squeeze(), "#02c59b")
+ax1.fill_between(
+    t,
+    np.mean(ev1.squeeze(), axis=0) - np.std(ev1.squeeze(), axis=0),
+    np.mean(ev1.squeeze(), axis=0) + np.std(ev1.squeeze(), axis=0),
+    color="#7c7979",
+)
+ax1.fill_between(
+    t,
+    np.mean(rev1.squeeze(), axis=0) - np.std(rev1.squeeze(), axis=0),
+    np.mean(rev1.squeeze(), axis=0) + np.std(rev1.squeeze(), axis=0),
+    color="#87d498",
+)
+ax1.plot(t, np.mean(ev1.squeeze(), axis=0), "k")
+ax1.plot(t, np.mean(rev1.squeeze(), axis=0), "#02c59b")
 ax1.set_xlabel("Time (h)")
 ax1.set_ylabel("Explained variance")
 ax1.legend(["EV", "REV"])
@@ -52,8 +64,20 @@ ax1.set_ylim([0, 0.3])
 
 ax2 = fig.add_subplot(gs[0, 1], sharey=ax1)
 sessions[1].brainstates.addBackgroundtoPlots(ax2)
-ax2.plot(t, ev2.squeeze(), "k")
-ax2.plot(t, rev2.squeeze(), "#02c59b")
+ax2.fill_between(
+    t,
+    np.mean(ev2.squeeze(), axis=0) - np.std(ev2.squeeze(), axis=0),
+    np.mean(ev2.squeeze(), axis=0) + np.std(ev2.squeeze(), axis=0),
+    color="#7c7979",
+)
+ax2.fill_between(
+    t,
+    np.mean(rev2.squeeze(), axis=0) - np.std(rev2.squeeze(), axis=0),
+    np.mean(rev2.squeeze(), axis=0) + np.std(rev2.squeeze(), axis=0),
+    color="#87d498",
+)
+ax2.plot(t, np.mean(ev2.squeeze(), axis=0), "k")
+ax2.plot(t, np.mean(rev2.squeeze(), axis=0), "#02c59b")
 ax2.set_xlabel("Time (h)")
 ax2.text(0.2, 0.28, "POST NSD", fontweight="bold")
 ax2.set_xlim([0, 10])
