ad79812088ab872b1f404e5c0afd57175fbfadf2
Bapun giri <bapung@MSV00125.med.umich.edu>
2020-05-24 21:52:47 -0400

    ripple and spindle correlogram

---
+++ oscillations/ripple_spindle.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+import seaborn as sns
+import matplotlib
+from collections import namedtuple
+
+cmap = matplotlib.cm.get_cmap("hot_r")
+
+
+from callfunc import processData
+
+basePath = [
+    # "/data/Clustering/SleepDeprivation/RatJ/Day1/",
+    # "/data/Clustering/SleepDeprivation/RatK/Day1/",
+    "/data/Clustering/SleepDeprivation/RatN/Day1/",
+    # "/data/Clustering/SleepDeprivation/RatJ/Day2/",
+    # "/data/Clustering/SleepDeprivation/RatK/Day2/",
+    # "/data/Clustering/SleepDeprivation/RatN/Day2/",
+    # "/data/Clustering/SleepDeprivation/RatK/Day4/"
+]
+
+
+sessions = [processData(_) for _ in basePath]
+
+tbin = lambda x: np.arange(x - 20, x + 20)
+for sub, sess in enumerate(sessions):
+
+    sess.trange = np.array([])
+    peakrpl = sess.ripple.peaktime
+    peakspndl = sess.spindle.peaktime
+
+    hist_rpl = [np.histogram(peakrpl, bins=tbin(_))[0] for _ in peakspndl]
+
+
+t = tbin(0)[:-1] + 0.5
+hist_rpl = np.asarray(hist_rpl).sum(axis=0)
+plt.plot(t, hist_rpl)
