f949e61acbf14fd0a0f1523da32cefb44ca1b57c
Bapun giri <bapung@MSV00125.med.umich.edu>
2020-05-25 22:44:41 -0400

    file rename

--- oscillations/osccillationDetection.py
+++ oscillations/osccillationDetection.py
@@ -5,6 +5,7 @@ import scipy.stats as stats
 import seaborn as sns
 import matplotlib
 from collections import namedtuple
+from pathlib import Path
 
 cmap = matplotlib.cm.get_cmap("hot_r")
 
@@ -12,9 +13,9 @@ cmap = matplotlib.cm.get_cmap("hot_r")
 from callfunc import processData
 
 basePath = [
-    # "/data/Clustering/SleepDeprivation/RatJ/Day1/",
+    "/data/Clustering/SleepDeprivation/RatJ/Day1/",
     # "/data/Clustering/SleepDeprivation/RatK/Day1/",
-    "/data/Clustering/SleepDeprivation/RatN/Day1/",
+    # "/data/Clustering/SleepDeprivation/RatN/Day1/",
     # "/data/Clustering/SleepDeprivation/RatJ/Day2/",
     # "/data/Clustering/SleepDeprivation/RatK/Day2/",
     # "/data/Clustering/SleepDeprivation/RatN/Day2/",
@@ -26,13 +27,20 @@ sessions = [processData(_) for _ in basePath]
 
 
 plt.clf()
-group = []
+
+# fig, ax = plt.subplots(figsize=(5, 3))
 for sub, sess in enumerate(sessions):
 
     sess.trange = np.array([])
+    # sess.brainstates.addBackgroundtoPlots(ax=ax)
     # sess.ripple.channels()
-    # sess.ripple._zscbestchannel(fromfile=0)
     # sess.ripple.detect()
-
     # sess.ripple.plot()
+    # sess.spindle.channels()
+    # sess.spindle.detect()
+    # sess.spindle.plot()
+    sess.swa.detect()
+    # sess.swa.plot()
     # _, b, c = sess.ripple.best_chan_lfp()
+
+# ax.set_xlim([-5, 10])
