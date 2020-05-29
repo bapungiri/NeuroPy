90767ce58b4a001b048634c836b5d7a3bbaed3b2
Bapun giri <bapung@MSV00125.med.umich.edu>
2020-05-28 23:22:56 -0400

    phase coupling multiple bands

--- oscillations/theta_gamma_phaseAmpcoupling_REM.py
+++ oscillations/theta_gamma_phaseAmpcoupling_REM.py
@@ -8,6 +8,7 @@ import seaborn as sns
 from signal_process import filter_sig, hilbertfast
 from statsmodels.tsa.stattools import grangercausalitytests
 import matplotlib as mpl
+from tensorpac import Pac
 
 from callfunc import processData
 
@@ -27,8 +28,11 @@ sessions = [processData(_) for _ in basePath]
 # during REM sleep
 plt.clf()
 fig = plt.figure(1, figsize=(1, 15))
-gs = GridSpec(1, 3, figure=fig)
+gs = GridSpec(4, 3, figure=fig)
 fig.subplots_adjust(hspace=0.5)
+
+
+# p = Pac(idpac=(6, 3, 0), f_pha=(4, 10, 1, 1), f_amp=(30, 100, 5, 5))
 
 for sub, sess in enumerate(sessions):
 
@@ -57,20 +61,19 @@ for sub, sess in enumerate(sessions):
 
     lfprem = np.asarray(lfprem)
 
+    # xpac = p.filterfit(1250.0, lfprem, n_perm=20)
     theta_lfp = stats.zscore(filter_sig.filter_theta(lfprem))
+    hil_theta = hilbertfast(theta_lfp)
+    theta_amp = np.abs(hil_theta)
+    theta_angle = np.angle(hil_theta, deg=True) + 180
+    angle_bin = np.arange(0, 360, 20)
+    bin_ind = np.digitize(theta_angle, bins=angle_bin)
 
-    for freq in freqIntervals:
-        gamma_lfp = stats.zscore(filter_sig.filter_gamma(lfprem))
+    for band, (lfreq, hfreq) in enumerate(freqIntervals):
+        gamma_lfp = stats.zscore(filter_sig.filter_cust(lfprem, lf=lfreq, hf=hfreq))
 
-        hil_theta = hilbertfast(theta_lfp)
         hil_gamma = hilbertfast(gamma_lfp)
-
-        theta_amp = np.abs(hil_theta)
         gamma_amp = np.abs(hil_gamma)
-
-        theta_angle = np.angle(hil_theta, deg=True) + 180
-        angle_bin = np.arange(0, 360, 20)
-        bin_ind = np.digitize(theta_angle, bins=angle_bin)
 
         mean_amp = np.zeros(len(angle_bin) - 1)
         for i in range(1, len(angle_bin)):
@@ -79,5 +82,38 @@ for sub, sess in enumerate(sessions):
 
         mean_amp_norm = mean_amp / np.sum(mean_amp)
 
-        ax = fig.add_subplot(gs[plt_ind])
+        ax = fig.add_subplot(gs[band + 1, plt_ind])
         ax.plot(angle_bin[:-1] + 10, mean_amp_norm, color=color)
+        # p.comodulogram(
+        #     xpac.mean(-1),
+        #     title="Contour plot with 5 regions",
+        #     cmap="Spectral_r",
+        #     plotas="contour",
+        #     ncontours=7,
+        # )
+
+plt.clf()
+ts = 2100
+interval = np.arange(ts * 1250, (ts + 2) * 1250)
+ax = fig.add_subplot(gs[0, :])
+lfpsample = lfprem[interval]
+troughs = sg.find_peaks(theta_angle[interval])[0]
+
+for i in troughs:
+    ax.plot([i, i], [-6000, 4000], "#EF9A9A")
+ax.plot(lfpsample, "k")
+ax.plot(filter_sig.filter_cust(lfpsample, lf=30, hf=50) - 2500, "#CE93D8")
+ax.plot(filter_sig.filter_cust(lfpsample, lf=50, hf=90) - 4000, "#1565C0")
+ax.plot(filter_sig.filter_cust(lfpsample, lf=100, hf=150) - 5500, "#E65100")
+# ax.plot(filter_sig.filter_cust(lfpsample, lf=4, hf=10), "#E65100")
+# ax.plot(theta_angle[interval], "#D500F9")
+ax.text(0.02, 0.83, "Raw LFP", fontsize=10, transform=plt.gcf().transFigure)
+ax.text(0.02, 0.79, "Low gamma (30-50 Hz)", fontsize=7, transform=plt.gcf().transFigure)
+ax.text(
+    0.02, 0.77, "High gamma (50-90 Hz)", fontsize=7, transform=plt.gcf().transFigure
+)
+ax.text(0.02, 0.75, "100-150 Hz", fontsize=7, transform=plt.gcf().transFigure)
+ax.set_xlim([0, 2 * 1250])
+ax.axis("off")
+
+fig.suptitle("Theta phase - gamma amplitude modulation during REM")
