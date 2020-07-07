e62c6f3e9f5b537bcf1d45c26f8e4ed533cbbaae
Bapun giri <bapung@MSV00125.med.umich.edu>
2020-07-06 20:49:12 -0400

    theta phases precession around place field started

--- placefieldCal/placefieldplots.py
+++ placefieldCal/placefieldplots.py
@@ -1,12 +1,14 @@
+#%%
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import scipy.stats as stats
-from matplotlib.gridspec import GridSpec
+import matplotlib.gridspec as gridspec
 from scipy.ndimage import gaussian_filter
 
 from callfunc import processData
 
+#%% Subjects
 basePath = [
     # "/data/Clustering/SleepDeprivation/RatJ/Day1/",
     # "/data/Clustering/SleepDeprivation/RatK/Day1/",
@@ -20,19 +22,46 @@ basePath = [
 
 sessions = [processData(_) for _ in basePath]
 
+#%% 2D place field in openfield arena
+# region
 plt.close("all")
 for sub, sess in enumerate(sessions):
 
     sess.trange = np.array([])
 
     # sess.spikes.fromCircus(fileformat="same_folder")
-    # sess.placefield.pf2d.compute()
-    # sess.placefield.pf2d.plotMap()
+    sess.placefield.pf2d.compute()
+    sess.placefield.pf2d.plotMap()
     # sess.placefield.pf2d.plotRaw()
-    sess.position.export2Neuroscope()
+    # sess.position.export2Neuroscope()
 #     sess.spikes.stability.firingRate()
 
 
 # stability = sess.spikes.stability.isStable
 # unstable = sess.spikes.stability.unstable
 # stable = sess.spikes.stability.stable
+
+# endregion
+
+#%% Theta phase precession Open field
+# region
+plt.clf()
+fig = plt.figure(1, figsize=(10, 15))
+gs = gridspec.GridSpec(8, 5, figure=fig)
+fig.subplots_adjust(hspace=0.3)
+for sub, sess in enumerate(sessions):
+
+    sess.trange = np.array([])
+
+    # sess.spikes.fromCircus(fileformat="same_folder")
+    sess.placefield.pf2d.compute()
+    sess.placefield.pf2d.plotMap()
+    sess.placefield.pf2d.plotRaw()
+    maps = sess.placefield.pf2d.maps
+    for cell, pfmap in enumerate(maps):
+        pfmap_linear = np.ravel(pfmap)
+        ax = fig.add_subplot(gs[cell])
+        ax.plot(pfmap_linear)
+
+
+# endregion
