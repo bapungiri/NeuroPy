4b599f4be899898730080768ce21224075d20863
Bapun giri <bapung@MSV00125.med.umich.edu>
2020-08-13 21:42:36 -0400

    refactor and improved expvar calculation

--- ModulesPath/replay.py
+++ ModulesPath/replay.py
@@ -104,73 +104,91 @@ class ExplainedVariance:
 
     nChans = 16
     binSize = 0.250  # in seconds
-    timeWindow = 3600  # in seconds
+    window = 900  # in seconds
 
     def __init__(self, obj):
         self._obj = obj
 
-    def compute(self, template, matchWind, control):
+    # TODO  smooth version of explained variance
+    def compute(self, template=None, match=None, control=None):
         """ Calucate explained variance (EV) and reverse EV
         References:
         1) Kudrimoti 1999
         2) Tastsuno et al. 2007
         """
 
-        pre = self._obj.epochs.pre
-        maze = self._obj.epochs.maze
-        post = self._obj.epochs.post
+        if None in [template, match, control]:
+            control = self._obj.epochs.pre
+            template = self._obj.epochs.maze
+            match = self._obj.epochs.post
 
+        # ----- choosing cells ----------------
         spks = self._obj.spikes.times
         stability = self._obj.spikes.stability.info
         stable_pyr = np.where((stability.q < 4) & (stability.stable == 1))[0]
         print(f"Calculating EV for {len(stable_pyr)} stable cells")
-
-        windowSize = self.timeWindow
         spks = [spks[_] for _ in stable_pyr]
 
-        pre_bin = np.arange(pre[0], pre[1], self.binSize)
-        maze_bin = np.arange(maze[0], maze[1], self.binSize)
-        post_bin = np.arange(post[0], post[1], self.binSize)
+        # ------- windowing the time periods ----------
+        window = self.window
+        nbins_window = int(window / self.binSize)
 
-        pre_spikecount = np.array([np.histogram(x, bins=pre_bin)[0] for x in spks])
-        pre_spikecount = [
-            pre_spikecount[:, i : i + windowSize]
-            for i in range(0, 3 * windowSize, windowSize)
-        ]
-        maze_spikecount = np.array([np.histogram(x, bins=maze_bin)[0] for x in spks])
-        post_spikecount = np.array([np.histogram(x, bins=post_bin)[0] for x in spks])
-        post_spikecount = [
-            post_spikecount[:, i : i + windowSize]
-            for i in range(0, 40 * windowSize, windowSize)
-        ]
+        # ---- function to calculate correlation in each window ---------
+        def cal_corr(period, windowing=True):
+            bin_period = np.arange(period[0], period[1], self.binSize)
+            spkcnt = np.array([np.histogram(x, bins=bin_period)[0] for x in spks])
 
-        # pre_corr = np.corrcoef(pre_spikecount)
-        pre_corr = [np.corrcoef(pre_spikecount[x]) for x in range(len(pre_spikecount))]
-        maze_corr = np.corrcoef(maze_spikecount)
-        post_corr = [
-            np.corrcoef(post_spikecount[x]) for x in range(len(post_spikecount))
-        ]
+            if windowing:
+                dur = np.diff(period)
+                nwindow = dur / window
+                t = np.arange(period[0], period[1], window)[:-1] + window / 2
 
-        # --- selecting only pairwise correlations from different shanks
+                window_spkcnt = [
+                    spkcnt[:, i : i + nbins_window]
+                    for i in range(0, int(nwindow) * nbins_window, nbins_window)
+                ]
+
+                if nwindow % 1 > 0.3:
+                    window_spkcnt.append(spkcnt[:, int(nwindow) * nbins_window :])
+                    t = np.append(t, round(nwindow % 1, 3) / 2)
+
+                corr = [
+                    np.corrcoef(window_spkcnt[x]) for x in range(len(window_spkcnt))
+                ]
+
+            else:
+                corr = np.corrcoef(spkcnt)
+                t = None
+
+            return corr, t
+
+        # ---- correlation for each time period -----------
+        control_corr, self.t_control = cal_corr(period=control)
+        template_corr, _ = cal_corr(period=template, windowing=False)
+        match_corr, self.t_match = cal_corr(period=match)
+
+        # ----- indices for cross shanks correlation -------
         shnkId = np.asarray(self._obj.spikes.info.shank)
         shnkId = shnkId[stable_pyr]
         assert len(shnkId) == len(spks)
         cross_shnks = np.nonzero(np.tril(shnkId.reshape(-1, 1) - shnkId.reshape(1, -1)))
-        # pre_corr = pre_corr[cross_shnks]
-        pre_corr = [pre_corr[x][cross_shnks] for x in range(len(pre_spikecount))]
-        print(len(pre_spikecount))
-        maze_corr = maze_corr[cross_shnks]
-        post_corr = [post_corr[x][cross_shnks] for x in range(len(post_spikecount))]
 
-        parcorr_maze_vs_post, rev_corr = parcorr_mult([maze_corr], post_corr, pre_corr)
+        # --- selecting only pairwise correlations from different shanks -------
+        control_corr = [control_corr[x][cross_shnks] for x in range(len(control_corr))]
+        template_corr = template_corr[cross_shnks]
+        match_corr = [match_corr[x][cross_shnks] for x in range(len(match_corr))]
 
-        ev_maze_vs_post = parcorr_maze_vs_post ** 2
+        parcorr_template_vs_match, rev_corr = parcorr_mult(
+            [template_corr], match_corr, control_corr
+        )
+
+        ev_template_vs_match = parcorr_template_vs_match ** 2
         rev_corr = rev_corr ** 2
 
-        self.ev = ev_maze_vs_post
+        self.ev = ev_template_vs_match
         self.rev = rev_corr
 
-    def plot(self, ax=None):
+    def plot(self, ax=None, tstart=0):
 
         ev_mean = np.mean(self.ev.squeeze(), axis=0)
         ev_std = np.std(self.ev.squeeze(), axis=0)
@@ -184,7 +202,7 @@ class ExplainedVariance:
             fig.subplots_adjust(hspace=0.3)
             ax = fig.add_subplot(gs[0])
 
-        t = (np.linspace(0, 40, 41) * 0.25)[1:] - 0.125
+        t = (self.t_match - tstart) / 3600  # converting to hour
 
         ax.fill_between(
             t, ev_mean - ev_std, ev_mean + ev_std, color="#7c7979",
@@ -193,13 +211,13 @@ class ExplainedVariance:
             t, rev_mean - rev_std, rev_mean + rev_std, color="#87d498",
         )
 
-        ax.plot(t,ev_meean, axis=0), "k")
+        ax.plot(t, ev_mean, "k")
         ax.plot(t, rev_mean, "#02c59b")
         ax.set_xlabel("Time (h)")
         ax.set_ylabel("Explained variance")
         ax.legend(["EV", "REV"])
         ax.text(0.2, 0.28, "POST SD", fontweight="bold")
-        ax.set_xlim([0, 10])
+        # ax.set_xlim([0, 4])
 
 
 class CellAssembly:
