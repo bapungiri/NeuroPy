f344a774f62732ac0fcd5998ae83e73de81b8282
Bapun giri <bapung@MSV00125.med.umich.edu>
2020-10-10 20:35:20 -0400

    minor mods

--- oscillations/test.py
+++ oscillations/test.py
@@ -1,45 +1,9 @@
-# %%
-import os
-import random
-import warnings
-from typing import Dict
+def append_to(element, to=[1,5]):
+    to.append(element)
+    return to
 
-# import ipywidgets as widgets
-import matplotlib as mpl
-import matplotlib.gridspec as gridspec
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import pingouin as pg
-import scipy.signal as sg
-import scipy.stats as stats
-import seaborn as sns
-import signal_process
+my_list = append_to(12)
+print(my_list)
 
-from callfunc import processData
-from mathutil import threshPeriods
-from plotUtil import Colormap, Fig
-from scipy import fft, interpolate
-from scipy.ndimage import gaussian_filter, gaussian_filter1d
-from signal_process import bicoherence
-from sklearn import linear_model
-from tables.description import Col
-
-# from artifactDetect import findartifact
-
-# warnings.simplefilter(action="default")
-#%% Subjects to choose from
-basePath = [
-    "/data/Clustering/SleepDeprivation/RatJ/Day1/",
-    # "/data/Clustering/SleepDeprivation/RatK/Day1/",
-    # "/data/Clustering/SleepDeprivation/RatN/Day1/",
-    # "/data/Clustering/SleepDeprivation/RatJ/Day2/",
-    # "/data/Clustering/SleepDeprivation/RatK/Day2/",
-    # "/data/Clustering/SleepDeprivation/RatN/Day2/",
-    # "/data/Clustering/SleepDeprivation/RatJ/Day4/",
-    # "/data/Clustering/SleepDeprivation/RatK/Day4/",
-    # "/data/Clustering/SleepDeprivation/RatN/Day4/",
-    # "/data/Clustering/SleepDeprivation/RatA14d1LP/Rollipram/",
-]
-sessions = [processData(_) for _ in basePath]
-
+my_other_list = append_to(42)
+print(my_other_list)
