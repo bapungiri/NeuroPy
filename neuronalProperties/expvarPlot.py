fc2410bd464ff126f4566505ac0e4391944582b2
bapungiri <bapun.k.giri@gmail.com>
2020-12-22 18:52:49 -0500

    moved

--- neuronalProperties/expvarPlot.py
+++ neuronalProperties/expvarPlot.py
@@ -5,41 +5,30 @@ import pandas as pd
 import scipy.stats as stats
 import matplotlib.gridspec as gridspec
 from scipy.ndimage import gaussian_filter
-
+import subjects
 from callfunc import processData
-
-basePath = [
-    # "/data/Clustering/SleepDeprivation/RatJ/Day1/",
-    "/data/Clustering/SleepDeprivation/RatK/Day1/",
-    "/data/Clustering/SleepDeprivation/RatN/Day1/",
-    # "/data/Clustering/SleepDeprivation/RatJ/Day2/",
-    # "/data/Clustering/SleepDeprivation/RatK/Day2/",
-    "/data/Clustering/SleepDeprivation/RatN/Day2/",
-    # "/data/Clustering/SleepDeprivation/RatK/Day4/",
-    # "/data/Clustering/SleepDeprivation/RatN/Day4/",
-]
-
-
-sessions = [processData(_) for _ in basePath]
+from plotUtil import Fig
 
 #%% Explained variance PRE-MAZE-POST
 # region
 """Only found stable units for 3 sessions
 """
-plt.clf()
-fig = plt.figure(1, figsize=(6, 10))
-gs = gridspec.GridSpec(3, 1, figure=fig)
-fig.subplots_adjust(hspace=0.4)
+figure = Fig()
+fig, gs = figure.draw(grid=(1, 1))
+sessions = subjects.sd([3])
 
 for sub, sess in enumerate(sessions):
 
     sess.trange = np.array([])
-    maze = sess.epochs.maze
+    pre = sess.epochs.pre
+    maze = sess.epochs.maze1
     post = sess.epochs.post
     bins = [
-        [maze[0], maze[1]],
-        [post[0] + 0 * 3600, post[0] + 1 * 3600],
-        [post[0] + 5 * 3600, post[0] + 8 * 3600],
+        pre,
+        [post[0] + 4 * 3600, post[0] + 5 * 3600],
+        [post[0] + 5 * 3600, post[0] + 10 * 4600],
+        # post,
+        # [post[0] + 5 * 3600, post[0] + 8 * 3600],
     ]
     sess.spikes.stability.firingRate(bins=bins)
     # sess.spikes.stability.refPeriodViolation()
@@ -50,11 +39,13 @@ for sub, sess in enumerate(sessions):
 
     ax1 = fig.add_subplot(axstate[1:])
     sess.replay.expvar.plot(ax=ax1, tstart=bins[2][0])
+    # ax1.spines["right"].set_visible("False")
+    # ax1.spines["top"].set_visible("False")
 
     axhypno = fig.add_subplot(axstate[0], sharex=ax1)
     sess.brainstates.hypnogram(ax1=axhypno, tstart=bins[2][0], unit="h")
     # panel_label(axhypno, "a")
-    # ax1.set_ylim([0, 0.3])
+    # ax1.set_ylim([0, 11])
 
 
 # endregion
@@ -87,7 +78,9 @@ for sub, sess in enumerate(sessions):
         # violations = sess.spikes.stability.violations
         sess.spikes.stability.firingRate(bins=bins)
         sess.replay.expvar.compute(
-            template=bins[1], match=bins[2], control=bins[0],
+            template=bins[1],
+            match=bins[2],
+            control=bins[0],
         )
 
         axstate = gridspec.GridSpecFromSubplotSpec(
