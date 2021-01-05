443aad17cebc1937a5c2e4c1f06ea9fe7b9e87fa
bapungiri <bapun.k.giri@gmail.com>
2021-01-04 19:00:13 -0500

    new analysis

--- neuronalProperties/expvarPlot.py
+++ neuronalProperties/expvarPlot.py
@@ -14,7 +14,7 @@ from plotUtil import Fig
 """Only found stable units for 3 sessions
 """
 figure = Fig()
-fig, gs = figure.draw(grid=(1, 1))
+fig, gs = figure.draw(num=1, grid=(1, 1))
 sessions = subjects.sd([3])
 
 for sub, sess in enumerate(sessions):
@@ -24,7 +24,9 @@ for sub, sess in enumerate(sessions):
     maze = sess.epochs.maze1
     post = sess.epochs.post
     bins = [
-        pre,
+        # pre,
+        maze,
+        # [post[0] + 0 * 3600, post[0] + 1 * 3600],
         [post[0] + 4 * 3600, post[0] + 5 * 3600],
         [post[0] + 5 * 3600, post[0] + 10 * 4600],
         # post,
@@ -38,12 +40,13 @@ for sub, sess in enumerate(sessions):
     axstate = gridspec.GridSpecFromSubplotSpec(4, 1, subplot_spec=gs[sub], hspace=0.2)
 
     ax1 = fig.add_subplot(axstate[1:])
-    sess.replay.expvar.plot(ax=ax1, tstart=bins[2][0])
+    sess.replay.expvar.plot(ax=ax1, tstart=bins[0][0])
+    ax1.set_xlim(left=0)
     # ax1.spines["right"].set_visible("False")
     # ax1.spines["top"].set_visible("False")
 
     axhypno = fig.add_subplot(axstate[0], sharex=ax1)
-    sess.brainstates.hypnogram(ax1=axhypno, tstart=bins[2][0], unit="h")
+    sess.brainstates.hypnogram(ax1=axhypno, tstart=bins[0][0], unit="h")
     # panel_label(axhypno, "a")
     # ax1.set_ylim([0, 11])
 
@@ -54,15 +57,12 @@ for sub, sess in enumerate(sessions):
 # region
 """Only found stable units for 3 sessions
 """
-plt.clf()
-fig = plt.figure(1, figsize=(6, 10))
-gs = gridspec.GridSpec(3, 5, figure=fig)
-fig.subplots_adjust(hspace=0.4, wspace=0.0)
-
+figure = Fig()
+fig, gs = figure.draw(num=1, grid=(2, 1))
+sessions = subjects.sd([3])
 for sub, sess in enumerate(sessions):
 
-    sess.trange = np.array([])
-    maze = sess.epochs.maze
+    maze = sess.epochs.maze1
     post = sess.epochs.post
     pre = sess.epochs.pre
 
@@ -104,9 +104,53 @@ for sub, sess in enumerate(sessions):
 
         axhypno = fig.add_subplot(axstate[0], sharex=ax1)
         sess.brainstates.hypnogram(ax1=axhypno, tstart=post[0], unit="h")
-        axhypno.set_title(sess.sessinfo.session.sessionName)
+        # axhypno.set_title(sess.sessinfo.session.sessionName)
     # panel_label(axhypno, "a")
     # ax1.set_ylim([0, 0.3])
+
+
+# endregion
+
+#%% Explained variance in recovery sleep
+# region
+figure = Fig()
+fig, gs = figure.draw(num=1, grid=(6, 1))
+sessions = subjects.Sd().ratSday3
+
+for sub, sess in enumerate(sessions):
+
+    sess.trange = np.array([])
+    pre = sess.epochs.pre
+    maze = sess.epochs.maze1
+    post = sess.epochs.post
+
+    template_periods = [
+        [post[0] + _ * 3600, post[0] + (_ + 1) * 3600] for _ in range(5)
+    ]
+
+    for i, period in enumerate(template_periods):
+        bins = [
+            maze,
+            period,
+            [post[0] + 5 * 3600, post[0] + 10 * 4600],
+        ]
+        sess.spikes.stability.firingRate(bins=bins)
+        sess.replay.expvar.compute(template=bins[1], match=bins[2], control=bins[0])
+
+        ax1 = fig.add_subplot(gs[i + 1])
+        sess.replay.expvar.plot(ax=ax1, tstart=post[0])
+        ax1.set_xlim(left=0)
+        ax1.set_ylim(top=0.35)
+        h = np.array(period) / 3600 - post[0] / 3600
+        ax1.axvspan(xmin=h[0], xmax=h[1], color="red", alpha=0.5)
+        # ax1.spines["right"].set_visible("False")
+        # ax1.spines["top"].set_visible("False")
+
+    axstate = gridspec.GridSpecFromSubplotSpec(4, 1, subplot_spec=gs[0], hspace=0.2)
+    axhypno = fig.add_subplot(axstate[3], sharex=ax1)
+    sess.brainstates.hypnogram(ax1=axhypno, tstart=post[0], unit="h")
+    # panel_label(axhypno, "a")
+    # ax1.set_ylim([0, 11])
 
 
 # endregion
