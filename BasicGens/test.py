253cbf32fbaa736f62caed5ee79aa192a738ea5a
bapungiri <bapun.k.giri@gmail.com>
2021-05-19 17:33:00 -0400

    minor mods

--- BasicGens/test.py
+++ BasicGens/test.py
@@ -1,58 +1,22 @@
-import matplotlib
-import sys
-
-# Make sure that we are using QT5
-matplotlib.use("Qt5Agg")
+import numpy as np
 import matplotlib.pyplot as plt
-from PyQt5 import QtWidgets
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+from scipy import stats
+from shapely.geometry import Point, LineString
+
+times = np.array([1.0, 2.0, 3.0])
+data = np.array([[0.0, 0.5], [0.5, 0.1], [1.0, 1.2]])
+
+# pos = nept.Position(data, times)
+
+line = LineString([(0.0, 0.0), (1.0, 1.0)])
+ideal_path = line
+zpos = []
+for point_x, point_y in zip(data[:, 0], data[:, 1]):
+    zpos.append(ideal_path.project(Point(point_x, point_y)))
+zpos = np.array(zpos)
 
 
-class ScrollableWindow(QtWidgets.QMainWindow):
-    def __init__(self, fig):
-        self.qapp = QtWidgets.QApplication([])
+# linear = pos.linearize(line)
 
-        QtWidgets.QMainWindow.__init__(self)
-        self.widget = QtWidgets.QWidget()
-        self.setCentralWidget(self.widget)
-        self.widget.setLayout(QtWidgets.QVBoxLayout())
-        self.widget.layout().setContentsMargins(0, 0, 0, 0)
-        self.widget.layout().setSpacing(0)
-
-        self.fig = fig
-        self.canvas = FigureCanvas(self.fig)
-        self.canvas.draw()
-        self.scroll = QtWidgets.QScrollArea(self.widget)
-        self.scroll.setWidget(self.canvas)
-
-        self.nav = NavigationToolbar(self.canvas, self.widget)
-        self.widget.layout().addWidget(self.nav)
-        self.widget.layout().addWidget(self.scroll)
-
-        self.show()
-        exit(self.qapp.exec_())
-
-
-def main(fig):
-    if not QtWidgets.QApplication.instance():
-        app = QtWidgets.QApplication(sys.argv)
-    else:
-        app = QtWidgets.QApplication.instance()
-    main = ScrollableWindow(fig)
-    main.show()
-
-    return main
-
-
-# if __name__ == "__main__":
-#     m = main()
-
-
-# create a figure and some subplots
-fig, axes = plt.subplots(ncols=4, nrows=5, figsize=(16, 16))
-for ax in axes.flatten():
-    ax.plot([2, 3, 5, 1])
-
-# pass the figure to the custom window
-a = main(fig)
+assert np.allclose(linear.x, np.array([0.35355339, 0.42426407, 1.41421356]))
+assert np.allclose(linear.time, np.array([1.0, 2.0, 3.0]))
