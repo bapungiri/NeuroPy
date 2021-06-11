33551fb2e42fff53501c6cb6e09d27600bc76dad
bapungiri <bapun.k.giri@gmail.com>
2021-06-11 16:58:37 -0400

    figure related

--- DataPaths/subjects.py
+++ DataPaths/subjects.py
@@ -1,4 +1,4 @@
-from ModulesPath.callfunc import processData
+from ModulesPath import ProcessData
 from typing import List
 
 colors = {"sd": "#ff6b6b", "nsd": "#69c"}
@@ -21,30 +21,30 @@ def allsess():
         "/data/Clustering/SleepDeprivation/RatN/Day4/",
         "/data/Clustering/SleepDeprivation/RatA14d1LP/Rollipram/",
     ]
-    return [processData(_) for _ in paths]
+    return [ProcessData(_) for _ in paths]
 
 
 class Of:
     @property
     def ratJday4(self):
         path = "/data/Clustering/SleepDeprivation/RatJ/Day4/"
-        return [processData(path)]
+        return [ProcessData(path)]
 
     @property
     def ratKday4(self):
         path = "/data/Clustering/SleepDeprivation/RatK/Day4/"
-        return [processData(path)]
+        return [ProcessData(path)]
 
     @property
     def ratNday4(self):
         path = "/data/Clustering/SleepDeprivation/RatN/Day4/"
-        return [processData(path)]
+        return [ProcessData(path)]
 
 
 class Sd:
     @property
     def allsess(self):
-        pipelines: List[processData]
+        pipelines: List[ProcessData]
         pipelines = (
             self.ratJday1
             + self.ratKday1
@@ -57,62 +57,72 @@ class Sd:
     @property
     def ratJday1(self):
         path = "/data/Clustering/SleepDeprivation/RatJ/Day1/"
-        return [processData(path)]
+        return [ProcessData(path)]
 
     @property
     def ratKday1(self):
         path = "/data/Clustering/SleepDeprivation/RatK/Day1/"
-        return [processData(path)]
+        return [ProcessData(path)]
 
     @property
     def ratNday1(self):
         path = "/data/Clustering/SleepDeprivation/RatN/Day1/"
-        return [processData(path)]
+        return [ProcessData(path)]
 
     @property
     def ratSday3(self):
         path = "/data/Clustering/SleepDeprivation/RatS/Day3SD/"
-        return [processData(path)]
+        return [ProcessData(path)]
 
     @property
     def ratRday2(self):
         path = "/data/Clustering/SleepDeprivation/RatR/Day2SD"
-        return [processData(path)]
+        return [ProcessData(path)]
+
+    @property
+    def utkuAG_day1(self):
+        path = "/data/Clustering/SleepDeprivation/Utku/AG_2019-12-22_SD_day1/"
+        return [ProcessData(path)]
+
+    @property
+    def utkuAG_day2(self):
+        path = "/data/Clustering/SleepDeprivation/Utku/AG_2019-12-26_SD_day2/"
+        return [ProcessData(path)]
 
     def __add__(self, other):
-        pipelines: List[processData] = self.allsess + other.allsess
+        pipelines: List[ProcessData] = self.allsess + other.allsess
         return pipelines
 
 
 class Nsd:
     @property
     def allsess(self):
-        pipelines: List[processData]
+        pipelines: List[ProcessData]
         pipelines = self.ratJday2 + self.ratKday2 + self.ratNday2 + self.ratSday2
         return pipelines
 
     @property
     def ratJday2(self):
         path = "/data/Clustering/SleepDeprivation/RatJ/Day2/"
-        return [processData(path)]
+        return [ProcessData(path)]
 
     @property
     def ratKday2(self):
         path = "/data/Clustering/SleepDeprivation/RatK/Day2/"
-        return [processData(path)]
+        return [ProcessData(path)]
 
     @property
     def ratNday2(self):
         path = "/data/Clustering/SleepDeprivation/RatN/Day2/"
-        return [processData(path)]
+        return [ProcessData(path)]
 
     @property
     def ratSday2(self):
         path = "/data/Clustering/SleepDeprivation/RatS/Day2NSD/"
-        return [processData(path)]
+        return [ProcessData(path)]
 
     def __add__(self, other):
-        pipelines: List[processData] = self.allsess + other.allsess
+        pipelines: List[ProcessData] = self.allsess + other.allsess
         return pipelines
 
 
@@ -126,7 +136,7 @@ class Tn:
     @property
     def ratSday5(self):
         path = "/data/Clustering/SleepDeprivation/RatS/Day5TwoNovel/"
-        return [processData(path)]
+        return [ProcessData(path)]
 
 
 # def sd(indx=None):
@@ -140,7 +150,7 @@ class Tn:
 #     ]
 #     if indx is not None:
 #         paths = [paths[_] for _ in indx]
-#     return [processData(_) for _ in paths]
+#     return [ProcessData(_) for _ in paths]
 
 
 # def nsd(indx=None):
@@ -153,4 +163,4 @@ class Tn:
 #     if indx is not None:
 #         paths = [paths[_] for _ in indx]
 
-#     return [processData(_) for _ in paths]
+#     return [ProcessData(_) for _ in paths]
