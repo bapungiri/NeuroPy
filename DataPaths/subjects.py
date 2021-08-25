2fcc0001063d7e9aa6c3b346c59c7f4f4b8356e0
bapungiri <bapun.k.giri@gmail.com>
2021-08-25 11:28:41 -0400

    minor stuff

--- DataPaths/subjects.py
+++ DataPaths/subjects.py
@@ -216,6 +216,11 @@ class Nsd:
         path = "/data/Clustering/sessions/RatR/Day1NSD/"
         return [ProcessData(path)]
 
+    @property
+    def ratUday2(self):
+        path = "/data/Clustering/sessions/RatU/RatUDay2NSD/"
+        return [ProcessData(path)]
+
     def __add__(self, other):
         pipelines: List[ProcessData] = self.allsess + other.allsess
         return pipelines
