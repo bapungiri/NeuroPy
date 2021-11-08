5d00cd4a8f7b2a6be74efc86ee952a46a15fedec
bapungiri <bapun.k.giri@gmail.com>
2021-11-08 14:47:00 -0500

    plot_in_bokeh as function for images

--- DataPaths/hfuncs.py
+++ DataPaths/hfuncs.py
@@ -1,7 +1,15 @@
-import signal_process
-from scipy import stats
+from base64 import b64encode
+from io import BytesIO
+
+import bokeh.models as bmodels
+import bokeh.plotting as bplot
 import numpy as np
 import pandas as pd
+from matplotlib import cm
+from neuropy.utils import signal_process
+from PIL import Image
+from scipy import stats
+from skimage import img_as_ubyte
 
 
 def wavelet_gamma_theta_phase(signal, theta_phase, binsize=9, frgamma=None, fs=1250):
@@ -28,3 +36,67 @@ def wavelet_gamma_theta_phase(signal, theta_phase, binsize=9, frgamma=None, fs=1
     gamma_at_theta.set_index("freq", inplace=True)
 
     return gamma_at_theta
+
+
+def to_png(arr):
+    out = BytesIO()
+    im = Image.fromarray(arr)
+    im.save(out, format="png")
+    return out.getvalue()
+
+
+def b64_image_files(images, colormap="hot"):
+    cmap = cm.get_cmap(colormap)
+    urls = []
+    for im in images:
+        im_ = np.flipud((im - np.min(im)) / np.max(im))
+        png = to_png(img_as_ubyte(cmap(im_)))
+        url = "data:image/png;base64," + b64encode(png).decode("utf-8")
+        urls.append(url)
+    return urls
+
+
+def plot_in_bokeh(x, y, img_arr, color_by=None, palette="jet", size=5):
+
+    # info to show on images
+    tooltips = """
+        <div>
+            <div>
+                <img
+                    src="@imgs" height="100" alt="@imgs" width="100"
+                    style="float: left; margin: 0px 15px 15px 0px;image-rendering: pixelated"
+                    border="2"
+                ></img>
+            </div>
+        </div>
+    """
+
+    arr_images = b64_image_files(img_arr)
+
+    if color_by is not None:
+        cmap = cm.get_cmap(palette)
+        color_by = color_by - np.min(color_by)
+        color_by /= np.max(color_by)
+        colors = cmap(color_by)
+    else:
+        colors = ["red"] * len(x)
+
+    source = bplot.ColumnDataSource(
+        data=dict(
+            x=x,
+            y=y,
+            imgs=arr_images,
+            colors=colors,
+        )
+    )
+
+    p = bplot.figure(
+        width=900,
+        height=500,
+        tooltips=tooltips,
+        x_axis_label="Time",
+        y_axis_label="Replay score",
+        title="Replay score over time, Hover to see posterior",
+    )
+    p.circle("x", "y", size=size, source=source, color="colors")
+    return p
