84082b67c5881f6459d27adaffe379609dd6f192
bapungiri <bapun.k.giri@gmail.com>
2022-02-01 16:53:19 -0500

    minor stuff

--- sleep_deprivation/sd_decoding.py
+++ sleep_deprivation/sd_decoding.py
@@ -4,14 +4,15 @@ import subjects
 from neuropy.analyses import Pf1D
 from neuropy.analyses import Decode1d
 
-sessions = subjects.nsd.ratUday2
+sessions = subjects.nsd.ratUday2 + subjects.sd.ratUday4
 
 for sub, sess in enumerate(sessions):
-    maze = sess.paradigm["maze"]
-    post = sess.paradigm["post"]
+    print(sess.animal.name)
+    maze = sess.paradigm["maze"].flatten()
+    post = sess.paradigm["post"].flatten()
     neurons = sess.neurons.get_neuron_type(neuron_type="pyr")
-    pos = sess.lin_maze
-    run = sess.run
+    pos = sess.maze
+    # run = sess.run
     pf = Pf1D(
         neurons=neurons,
         position=pos,
@@ -21,26 +22,33 @@ for sub, sess in enumerate(sessions):
         # epochs=run,
         frate_thresh=1,
     )
-    pf_neurons = neurons.get_by_id(pf.ratemap.neuron_ids)
-    epochs = sess.pbe.time_slice(post[0], post[1])
+    pf_neurons = neurons.get_by_id(pf.neuron_ids)
+    epochs = sess.pbe.time_slice(post[0], post[0] + 2 * 3600)
     decode = Decode1d(
-        neurons=pf_neurons, ratemap=pf.ratemap, epochs=epochs, bin_size=0.02
+        neurons=pf_neurons,
+        ratemap=pf,
+        epochs=epochs,
+        bin_size=0.02,
+        decode_margin=15,
+        nlines=5000,
     )
-    decode.n_jobs = 6
-    decode.calculate_shuffle_score(method="neuron_id", n_iter=1000)
+    decode.n_jobs = 5
+    decode.calculate_shuffle_score(method="neuron_id", n_iter=200)
 
     df = pd.DataFrame(
         {
-            "is_replay": decode.p_value < 0.01,
-            "is_forward": (decode.slope > 0) & (decode.p_value < 0.01),
-            "is_reverse": (decode.slope < 0) & (decode.p_value < 0.01),
-            "replay_p_value": decode.p_value,
-            "replay_score": decode.score,
-            "replay_slope": decode.slope,
+            "is_replay": decode.p_value < 0.05,
+            "is_forward": decode.velocity > 0,
+            "is_reverse": decode.velocity < 0,
+            "p_value": decode.p_value,
+            "score": decode.score,
+            "velocity": decode.velocity,
+            "intercept": decode.intercept,
         }
     )
-    new_epochs = epochs.add_column("replay_p_value", decode.p_value)
     new_epochs = epochs.add_dataframe(df)
-    new_epochs.metadata = {"posterior": decode.posterior}
-    new_epochs.filename = sess.filePrefix.with_suffix(".replay.pbe")
-    new_epochs.save()
+    new_epochs.metadata = {
+        "posterior": decode.posterior,
+        "shuffle_score": decode.shuffle_score,
+    }
+    new_epochs.save(sess.filePrefix.with_suffix(".replay.pbe"))
